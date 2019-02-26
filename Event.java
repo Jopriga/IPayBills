8e4a4baf300e515e71b5356cc0708c1722eb2409
Jopriga <jopriga@gmail.com>
2019-02-26 16:31:27 +0100

    Create class Event
    Represents a single Event which has a name and a number of participants

---
+++ Event.java
@@ -0,0 +1,35 @@
+package com.jorge.ipaybillsproyect.ipaybillsjava;
+
+public class Event {
+    private String name;
+    private int maxPeople;
+
+    public Event(String name, int maxPeople){
+        this.name = name;
+        this.maxPeople = maxPeople;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxPeople() {
+        return maxPeople;
+    }
+
+    public void setMaxPeople(int maxPeople) {
+        this.maxPeople = maxPeople;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "name='" + name + '\'' +
+                ", maxPeople=" + maxPeople +
+                '}';
+    }
+}
