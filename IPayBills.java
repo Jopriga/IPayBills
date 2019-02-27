d33f5b78228c849d1115cdf8da7e0e0239e95101
Jopriga <jopriga@gmail.com>
2019-02-27 20:23:15 +0100

    Add and remove events and people

--- IPayBills.java
+++ IPayBills.java
@@ -6,16 +6,35 @@ public class IPayBills {
 
     public static void main(String args[]){
         
-        EventInterface events = new Events(new ArrayList<Event>());
+        EventInterface events = Events.getEventList(new ArrayList<Event>());
 
-        events.addEvent(new Event("fiesta",4));
-        events.addEvent(new Event("cena",2));
-        events.addEvent(new Event("compra",3));
-        events.addEvent(new Event("viaje",4));
-        events.removeEvent(events.findByName("fiesta"););
-        events.addEvent(new Event("comida",5));
+        events.addEvent(new Event("fiesta",4,new PeopleList(new ArrayList<Person>())));
+        events.addEvent(new Event("cena",2,new PeopleList(new ArrayList<Person>())));
+        events.addEvent(new Event("compra",3,new PeopleList(new ArrayList<Person>())));
+        events.addEvent(new Event("viaje",4,new PeopleList(new ArrayList<Person>())));
+        
+        events.findByName("fiesta").getPeople().addPerson(new Person("jorge"));
+        events.findByName("fiesta").getPeople().addPerson(new Person("javain"));
+        events.findByName("fiesta").getPeople().addPerson(new Person("leo"));
+        events.findByName("fiesta").getPeople().addPerson(new Person("luis"));
+        
+        events.findByName("cena").getPeople().addPerson(new Person("jorge"));
+        events.findByName("cena").getPeople().addPerson(new Person("javain"));
+        events.findByName("cena").getPeople().addPerson(new Person("leo"));
+        events.findByName("cena").getPeople().addPerson(new Person("luis"));
+
+        events.removeEvent(events.findByName("compra"));
+        events.findByName("cena").getPeople().removePerson(events.findByName("cena").getPeople().findByName("leo"));
+        events.findByName("cena").getPeople().removePerson(events.findByName("cena").getPeople().findByName("javain"));
+
+        events.addEvent(new Event("viajesss",4,new PeopleList(new ArrayList<Person>())));
         
         for(int i=0;i<events.size();i++){
             System.out.println(events.findById(i).toString());
+            
+            for(int j=0;j<events.findById(i).getPeople().size();j++){
+            	System.out.println(events.findById(i).getPeople().findById(j).toString());
+            }
+            System.out.println();
         }
     }
