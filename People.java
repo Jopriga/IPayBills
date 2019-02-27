36adb601555b1a565a4b18ba7d9100c279167744
Jopriga <jopriga@gmail.com>
2019-02-27 20:01:40 +0100

    Create class People

---
+++ People.java
@@ -0,0 +1,36 @@
+package com.jorge.project.ipaybills;
+import java.util.ArrayList;
+
+public class People implements PersonInterface {
+	private ArrayList<Person>people;
+	
+	public People(ArrayList<Person>people){
+		this.people = people;
+	}
+	
+	public void addPerson(Person person){
+		this.people.add(person);
+	}
+	
+	public void removePerson(Person person){
+		people.remove(person);
+	}
+
+	public Person findByName(String name){
+        int aux = 0;
+        for (int i=0; i<people.size(); i++){
+            if(name.equals(people.get(i).getName())){
+                aux = i;
+            }
+        }
+        return findById(aux);
+    }
+
+    public Person findById(int id){
+        return people.get(id);
+    }
+
+    public int size(){
+        return people.size();
+    }
+}
