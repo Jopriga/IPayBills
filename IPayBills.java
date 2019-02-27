96589bcb2f9aa7f1f661291153fb59e261ce3b75
Jopriga <jopriga@gmail.com>
2019-02-27 13:49:06 +0100

    Update the methods changed in Events

--- IPayBills.java
+++ IPayBills.java
@@ -5,18 +5,17 @@ import java.util.ArrayList;
 public class IPayBills {
 
     public static void main(String args[]){
+        
         EventInterface events = new Events(new ArrayList<Event>());
 
         events.addEvent(new Event("fiesta",4));
         events.addEvent(new Event("cena",2));
         events.addEvent(new Event("compra",3));
         events.addEvent(new Event("viaje",4));
-
-        Event eventRemove=events.find("fiesta");
-        events.removeEvent(eventRemove);
+        events.removeEvent(events.findByName("fiesta"););
         events.addEvent(new Event("comida",5));
         
         for(int i=0;i<events.size();i++){
-            System.out.println("Event: "+events.getEvents().get(i).toString());
+            System.out.println(events.findById(i).toString());
         }
     }
