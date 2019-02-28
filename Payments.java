4437c6b2dd890c3d233dc0781d0787e2c15fa394
Jopriga <jopriga@gmail.com>
2019-02-28 19:59:57 +0100

    Create class Payments

---
+++ Payments.java
@@ -0,0 +1,21 @@
+package com.jorge.project.ipaybills;
+
+import java.util.ArrayList;
+
+public class Payments implements PaymentInterface{
+	private ArrayList<Payment>payments;
+	
+	public Payments(ArrayList<Payment>payments){
+		this.payments = payments;
+	}
+	
+	public void add(Payment payment){
+		payments.add(payment);
+		payment.getFrom().pay(payment.getMoney());
+		payment.getTo().receive(payment.getMoney());
+	}
+	
+    public Payment find(int id){
+        return payments.get(id);
+    }
+}
