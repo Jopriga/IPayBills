92119eaf521a6a4d1536d3befdbe723dae3de1fa
Jopriga <jopriga@gmail.com>
2019-02-28 18:22:07 +0100

    Correction interface name

--- MovementList.java
+++ MovementList.java
@@ -1,7 +1,7 @@
 package com.jorge.project.ipaybills;
 
-public interface MovementList {
-	Movement findById(int id);
+public interface MovementInterface {
+    Movement findById(int id);
     Movement findByName(String name);
     void add(Movement m); 
     void remove(Movement m);
