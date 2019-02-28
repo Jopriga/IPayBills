51456412cbe04c24da1d41a4c51152d25cce5910
Jopriga <jopriga@gmail.com>
2019-02-28 12:50:53 +0100

    Stupid fail

--- IPayBills.java
+++ IPayBills.java
@@ -7,7 +7,12 @@ public class IPayBills {
     public static void main(String args[]){
         
         EventInterface events = Events.getEventList();
-
+        
+        events.addEvent(new Event("fiesta",4,new PeopleList(new ArrayList<Person>()),new MovementList(new ArrayList<Movement>())));
+        events.addEvent(new Event("cena",2,new PeopleList(new ArrayList<Person>()),new MovementList(new ArrayList<Movement>())));
+        events.addEvent(new Event("compra",3,new PeopleList(new ArrayList<Person>()),new MovementList(new ArrayList<Movement>())));
+        events.addEvent(new Event("viaje",4,new PeopleList(new ArrayList<Person>()),new MovementList(new ArrayList<Movement>())));
+        
         events.findByName("fiesta").addPerson("jorge");
         events.findByName("fiesta").addPerson("javain");
         events.findByName("fiesta").addPerson("leo");
