713cd212361bbc4a18b823c41cc017ef865c8a7f
Jopriga <jopriga@gmail.com>
2019-02-28 18:23:02 +0100

    Delete MovementList interface

--- MovementList.java
+++
@@ -1,9 +0,0 @@
-package com.jorge.project.ipaybills;
-
-public interface MovementInterface {
-    Movement findById(int id);
-    Movement findByName(String name);
-    void add(Movement m); 
-    void remove(Movement m);
-    int size();
-}
