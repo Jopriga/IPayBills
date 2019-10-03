8f50ad41a03dbbcb04801e17ddc174b698ca9d66
Jopriga <jopriga@gmail.com>
2019-10-03 18:28:15 +0200

    Update Events

--- Events.java
+++ Events.java
@@ -1,19 +1,22 @@
-package com.jorge.project.ipaybills;
+
+package com.jorge.project.ipaybills.api;
 import java.util.ArrayList;
 
 public class Events implements EventInterface {
+    // private "file android object" that will be initialized
     private ArrayList<Event>events;
     private static Events object;
 
+    // Private constructor for singleton pattern
     private Events(){
-        events = new ArrayList<Event>();
+        events = new ArrayList<>(); // Receive information from file stored in Android file system
     }
-    
+
     public static Events getEventList(){
-    	 if (object == null){
- 	        object = new Events();
- 	    }
- 	    return object;
+        if (object == null){
+            object = new Events();
+        }
+        return object;
     }
 
     public void addEvent(Event event){
@@ -28,17 +31,25 @@ public class Events implements EventInterface {
         int aux = 0;
         for (int i=0; i<events.size(); i++){
             if(event.equals(events.get(i).getName())){
-               aux = i;
+                aux = i;
             }
         }
         return findById(aux);
     }
-    
-     public Event findById(int id){
+
+    public Event findById(int id){
         return events.get(id);
     }
-    
+
+    public String[] eventsToString(){
+        String []eventsString = new String[events.size()];
+        for(int i=0; i<events.size();i++){
+            eventsString[i] = events.get(i).getName();
+        }
+        return eventsString;
+    }
+
     public int size(){
         return events.size();
-    }    
+    }
 }
