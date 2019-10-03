a117f8bc8e76fa5300d52a11ef872fe361897011
Jopriga <jopriga@gmail.com>
2019-10-03 18:30:06 +0200

    Update EventInterface

--- EventInterface.java
+++ EventInterface.java
@@ -1,10 +1,11 @@
-package com.jorge.project.ipaybills;
+
+package com.jorge.project.ipaybills.api;
 
 public interface EventInterface {
-    
-    Event findByName(String name);
     Event findById(int id);
+    Event findByName(String name);
     void addEvent(Event e); //
     void removeEvent(Event e);
+    String[] eventsToString();
     int size();
 }
